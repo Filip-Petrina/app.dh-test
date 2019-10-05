6df7e4859afb61dde09ef3a014daea2cd028395f
filipp <filipp@nivas.hr>
2019-10-05 15:39:12 +0200

    reverted some thing

--- src/app/service/express/express.service.ts
+++ src/app/service/express/express.service.ts
@@ -9,20 +9,20 @@ import { environment } from '@env/environment';
 @Injectable()
 export class ExpressService {
 
-constructor(
-	private _http: HttpClient,
-) { }
+	constructor(
+		private _http: HttpClient,
+	) { }
 
 
-post(body, params:string = '') {
-	
-	
-	if(environment.debug) console.log('post()', environment.API_URL + params);
-	
-	return this._http.post(environment.API_URL + params, JSON.stringify(body))
-		.pipe(
-			map((res: any) => res)
-		)
+	post(body, params:string = '') {
+		
+		
+		if(environment.debug) console.log('post()', environment.API_URL + params);
+		
+		return this._http.post(environment.API_URL + params, JSON.stringify(body))
+			.pipe(
+				map((res: any) => res)
+			)
 		
 	}
 	
